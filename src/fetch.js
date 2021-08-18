f7bed55e2112262976ef84d5a2cd92c8064f0db0
Sumudu Herath <sumudu.herath@circles.asia>
2021-08-18 08:08:24 +0530

    handled api errors

--- src/fetch.js
+++ src/fetch.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { user_url } from "./routesClass";
-//  const email = "admin@circles.asia" ;
-//   const password = "circles111";
-// const fData = { email,password};
+
 
 
 export const getData = (email, password) => {
@@ -14,7 +12,7 @@ export const getData = (email, password) => {
     },
     body: JSON.stringify({ email, password })
 
-    //password:password// Adding body or contents to send
+    // Adding body or contents to send
   }) //servce file
     .then((response) => {
       if (response.status >= 200 && response.status <= 300) {
@@ -36,11 +34,12 @@ export const configFetchData = () => {
   return fetch(user_url + "/app-settings")
     .then((response) => response.json())
     .then(function (response) {
-      console.log(response);
+      console.log("Initial values : ", response);
+      //console.log(response);
       let configObj = {
-        login_Header: response.login.login_Header,
-        login_SubHeader: response.login.login_SubHeader,
-        dashboard_Header: response.dashboard.dashboard_Header,
+        login_Header: response.login?.login_Header,
+        login_SubHeader: response.login?.login_SubHeader,
+        dashboard_Header: response.dashboard?.dashboard_Header,
       };
       console.log("VALUES", configObj);
       return configObj;
@@ -49,47 +48,12 @@ export const configFetchData = () => {
     })
     .catch(function (error) {
       console.log(error);
+      return {
+        login_Header: "",
+        login_SubHeader: "",
+        dashboard_Header: "",
+      };
     });
 };
-
-
-
-
-
-// const axios = require("axios");
-
-// export const configFetchData = () => {
-//   return axios
-//     .get(routesClass.user_url + "/app-settings", {})
-//     .then(function (response) {
-//       console.log(response);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// };
-// export const configFetchData = () => {
-//   return fetch(routesClass.user_url + "/app-settings", {})
-//     .then(function (response) {
-//       console.log(response);
-//       let configObj = {
-//         login_Header: response.data.login.login_Header,
-//         login_SubHeader: response.login.login_SubHeader,
-//         dashboard_Header: response.dashboard.dashboard_Header,
-//       };
-//       console.log("VALUES", configObj);
-//       return configObj;
-
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// };
-
-
-
-
-
-
 
 
